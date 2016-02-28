c88fedcd27ae87e24104f294a05939386855ddcb
Roman <claprood.8@osu.edu>
2016-02-28 16:53:13 -0500

    pushing what I have

--- buffer_window.c
+++ buffer_window.c
@@ -7,14 +7,16 @@
 #include <errno.h>
 #include <stdlib.h>
 
-char servBuffer[64000];
-char cliBuffer[64000];
+#define MAX_BUFF 64000
+char servBuffer[MAX_BUFF];
+char cliBuffer[MAX_BUFF];
 
-char *servStart = 0;
-char *servEnd = 0;
-char *cliStart = 0;
-char *cliEnd = 0;
-int isBuffFull = 0;
+char *servStart = &servBuffer[0];
+char *servEnd = servStart;
+char *cliStart = &cliBuffer[0];
+char *cliEnd = cliStart;
+int isBuffFull = 0; // Set to 1 if  full, 0 not full
+int bytesInBuff = 0; // used to see if buffer should be set to full, incremented by # of bytes added to buffer
 
 typedef struct Pkt_Info{
 	int seq_num;
@@ -26,12 +28,25 @@ typedef struct Pkt_Info{
 Pkt_Info serWindow[20];
 Pkt_Info cliWindow[20];
 
-void writeToBuffer(){
+int writeToBufferC(int bytesToWrite, char pktBuffer[]){
+	int bytesWritten = 0;
 	if(isBuffFull == 1){
 		printf("The buffer is full!\n");
 	}else{
-	
+		if(isBuffFull == 0 && (cliEnd + bytesToWrite) >= cliStart){
+			bytesInBuff += bytesToWrite;
+			memcpy(&cliBuffer,&pktBuffer,bytesToWrite);
+			cliEnd += bytesInBuff;
+		}
 	}
+
+void readFromBufferC();
+
+int isBuffFilled(){
+	if(bytesInBuff == MAX_BUFF){
+		isBuffFull = 1;
+		return 1;
+	}
+	return 0;
 }
-void readFromBuffer();
 
